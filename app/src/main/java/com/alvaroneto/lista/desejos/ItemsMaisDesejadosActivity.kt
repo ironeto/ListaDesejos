1cca55fa3fe8f39f1bdf56a504ab659c1e434616
alvaroaugustodemarconeto <alvaro.marco.neto@gmail.com>
2023-03-27 22:35:38 +0200

    ad mob

--- app/src/main/java/com/alvaroneto/lista/desejos/ItemsMaisDesejadosActivity.kt
+++ app/src/main/java/com/alvaroneto/lista/desejos/ItemsMaisDesejadosActivity.kt
@@ -1,32 +1,33 @@
 package com.alvaroneto.lista.desejos
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import com.alvaroneto.lista.desejos.fragments.ListFragment
-import com.alvaroneto.lista.desejos.fragments.SenhaDificuldade
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.alvaroneto.lista.desejos.fragments.WeatherFragments
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class ItemsMaisDesejadosActivity : AppCompatActivity() {
     val uid = FirebaseAuth.getInstance().currentUser?.uid
     val ref = FirebaseDatabase.getInstance().getReference("/more_wished_items")
-
+    lateinit var adView: AdView
+    lateinit var adRequest: AdRequest
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_items_mais_desejados)
+
+        MobileAds.initialize(this)
+        adView = findViewById(R.id.adView)
+        adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+
 
         var listView = supportFragmentManager.findFragmentById(R.id.listViewTasks) as ListFragment
 
